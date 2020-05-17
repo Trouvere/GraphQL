67fc46d1f3b3eac3eab4cd6a55160f5a11aeefc9
Mishyn Aliaksei <trouvere777@mail.ru>
2020-05-17 16:13:05 +0300

    6.Migration to mongodb 7.Adding Mongoose Models & Updating Resolve

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -9,28 +9,46 @@ const {
   GraphQLList,
 } = graphql;
 
-const movies = [
-  { id: "1", name: "Pulp Fiction", genre: "Crime", directorId: "1" },
-  { id: "2", name: "1984", genre: "Sci-Fi", directorId: "2" },
-  { id: "3", name: "V for vendetta", genre: "Sci-Fi-Triller", directorId: "3" },
-  { id: "4", name: "Snatch", genre: "Crime-Comedy", directorId: "4" },
-  { id: "5", name: "Reservoir Dogs", genre: "Crime", directorId: "1" },
-  { id: "6", name: "The Hateful Eight", genre: "Crime", directorId: "1" },
-  { id: "7", name: "Inglourious Basterds", genre: "Crime", directorId: "1" },
-  {
-    id: "7",
-    name: "Lock, Stock and Two Smoking Barrels",
-    genre: "Crime-Comedy",
-    directorId: "4",
-  },
-];
+const Movies = require("../models/movie");
+const Directors = require("../models/director");
 
-const directors = [
-  { id: "1", name: "Quentin Tarantino", age: 55 },
-  { id: "2", name: "Michael Radford", age: 72 },
-  { id: "3", name: "James McTeigue", age: 51 },
-  { id: "4", name: "Guy Ritchie", age: 50 },
+/*
+// All IDs set automatically by mLab
+// Don't forget to update after creation
+const directorsJson = [
+  { "name": "Quentin Tarantino", "age": 55 }, // 5ec03355bd04d04e300ff021
+  { "name": "Michael Radford", "age": 72 }, // 5ec12fa572b07cb58afd636e
+  { "name": "James McTeigue", "age": 51 }, // 5ec1314b72b07cb58afd636f
+  { "name": "Guy Ritchie", "age": 50 }, // 5ec131ca72b07cb58afd6370
 ];
+// directorId - it is ID from the directors collection
+const moviesJson = [
+  { "name": "Pulp Fiction", "genre": "Crime", "directorId": "5ec03355bd04d04e300ff021" },
+  { "name": "1984", "genre": "Sci-Fi", "directorId": "5ec12fa572b07cb58afd636e" },
+  { "name": "V for vendetta", "genre": "Sci-Fi-Triller", "directorId": "5ec1314b72b07cb58afd636f" },
+  { "name": "Snatch", "genre": "Crime-Comedy", "directorId": "5ec131ca72b07cb58afd6370" },
+  { "name": "Reservoir Dogs", "genre": "Crime", "directorId": "5ec03355bd04d04e300ff021" },
+  { "name": "The Hateful Eight", "genre": "Crime", "directorId": "5ec03355bd04d04e300ff021" },
+  { "name": "Inglourious Basterds", "genre": "Crime", "directorId": "5ec03355bd04d04e300ff021" },
+  { "name": "Lock, Stock and Two Smoking Barrels", "genre": "Crime-Comedy", "directorId": "5ec131ca72b07cb58afd6370" },
+];
+const movies = [
+  { id: '1', name: "Pulp Fiction", genre: "Crime", directorId: "1" },
+  { id: '2', name: "1984", genre: "Sci-Fi", directorId: "2" },
+  { id: '3', name: "V for vendetta", genre: "Sci-Fi-Triller", directorId: "3" },
+  { id: '4', name: "Snatch", genre: "Crime-Comedy", directorId: "4" },
+  { id: '5', name: "Reservoir Dogs", genre: "Crime", directorId: "1" },
+  { id: '6', name: "The Hateful Eight", genre: "Crime", directorId: "1" },
+  { id: '7', name: "Inglourious Basterds", genre: "Crime", directorId: "1" },
+  { id: '8', name: "Lock, Stock and Two Smoking Barrels", genre: "Crime-Comedy", directorId: "4" },
+];
+const directors = [
+	{ id: '1', name: "Quentin Tarantino", age: 55 },
+  { id: '2', name: "Michael Radford", age: 72 },
+  { id: '3', name: "James McTeigue", age: 51 },
+  { id: '4', name: "Guy Ritchie", age: 50 },
+];
+*/
 
 const MovieType = new GraphQLObjectType({
   name: "Movie",
@@ -41,7 +59,8 @@ const MovieType = new GraphQLObjectType({
     director: {
       type: DirectorType,
       resolve(parent, args) {
-        return directors.find((director) => director.id === parent.id);
+        // return directors.find(director => director.id === parent.id);
+        return Directors.findById(parent.directorId);
       },
     },
   }),
@@ -56,7 +75,8 @@ const DirectorType = new GraphQLObjectType({
     movies: {
       type: new GraphQLList(MovieType),
       resolve(parent, args) {
-        return movies.filter((movie) => movie.directorId === parent.id);
+        // return movies.filter(movie => movie.directorId === parent.id);
+        return Movies.find({ directorId: parent.id });
       },
     },
   }),
@@ -69,26 +89,30 @@ const Query = new GraphQLObjectType({
       type: MovieType,
       args: { id: { type: GraphQLID } },
       resolve(parent, args) {
-        return movies.find((movie) => movie.id == args.id);
+        // return movies.find(movie => movie.id === args.id);
+        return Movies.findById(args.id);
       },
     },
     director: {
       type: DirectorType,
       args: { id: { type: GraphQLID } },
       resolve(parent, args) {
-        return directors.find((director) => director.id === args.id);
+        // return directors.find(director => director.id === args.id);
+        return Directors.findById(args.id);
       },
     },
     movies: {
       type: new GraphQLList(MovieType),
       resolve(parent, args) {
-        return movies;
+        // return movies;
+        return Movies.find({});
       },
     },
     directors: {
       type: new GraphQLList(DirectorType),
       resolve(parent, args) {
-        return directors;
+        // return directors;
+        return Directors.find({});
       },
     },
   },
--- server/server/app.js
+++ server/server/app.js
@@ -1,9 +1,15 @@
 const express = require("express");
 const graphqlHTTP = require("express-graphql");
 const schema = require("../schema/schema");
+const mongoose = require("mongoose");
 
 const app = express();
 const PORT = 3005;
+
+//Устанавливаем соединение с mongoose
+var mongoDB =
+  "mongodb+srv://Trouvere:inHaos666@cluster0-zfyme.mongodb.net/graphql-tutorial?retryWrites=true&w=majority"; //замените url!!!
+mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
 
 app.use(
   "/graphql",
@@ -12,6 +18,10 @@ app.use(
     graphiql: true,
   })
 );
+
+const dbConnection = mongoose.connection;
+dbConnection.on("error", (err) => console.log(`Connection error: ${err}`));
+dbConnection.once("open", () => console.log("Connected to DB!"));
 
 app.listen(PORT, (err) => {
   err ? console.log(err) : console.log("Server started!");
